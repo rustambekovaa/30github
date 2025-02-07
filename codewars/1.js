acf8834018d4aadf137416f7fb94f49086911d44
rustambekova <rustambekova@MacBook-Pro.local>
2025-02-07 23:52:41 +0600

    d1

---
+++ codewars/1.js
@@ -0,0 +1,22 @@
+
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit, there are 3 multiplications)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2, there are 4 multiplications)
+// 4 --> 0 (because 4 is already a one-digit number, there is no multiplication)
+
+function persistence(num) {
+    let a = num.toString()
+    let a2 = 0
+    while (a.length > 1) {
+        let product = 1;
+        for (i in a) {
+            product *= parseInt(a[i])
+        }
+        a = product.toString()
+        a2++
+    }
+    a1 = 1
+
+    return a2
+}
+console.log(persistence(999));
