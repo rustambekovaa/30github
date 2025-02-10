0617ad60756bd54689411aca2af21d96b4aca52f
rustambekova <rustambekovaa15@gmail.com>
2025-02-10 22:06:44 +0600

    thir_commit

--- codewars/1.js
+++ codewars/1.js
@@ -24,30 +24,30 @@
 
 //////////2222222222///////////
 
-var solution = function (firstArray, secondArray) {
-    let a = []
-  
-    for(let i = 0; i< firstArray.length;i++){
-        for(let j = 0; j< secondArray.length;j++){
-            if( i == j ){
-                if( firstArray[i] >= secondArray[j]  ){
-                    a.push((firstArray[i] - secondArray[j])**2)
-                    
-                }
-                else {
-                    a.push(( secondArray[j] - firstArray[i] )**2)
-                }
-            }
-           
-        }
-    }
-    let a2 =   a.reduce((acc, val) => {
-        return acc + val
-    })
-    console.log(a2 / a.length);
+// var solution = function (firstArray, secondArray) {
+//     let a = []
 
-}
-solution([10, 20, 10, 2], [10, 25, 5, -2]  )
+//     for(let i = 0; i< firstArray.length;i++){
+//         for(let j = 0; j< secondArray.length;j++){
+//             if( i == j ){
+//                 if( firstArray[i] >= secondArray[j]  ){
+//                     a.push((firstArray[i] - secondArray[j])**2)
+
+//                 }
+//                 else {
+//                     a.push(( secondArray[j] - firstArray[i] )**2)
+//                 }
+//             }
+
+//         }
+//     }
+//     let a2 =   a.reduce((acc, val) => {
+//         return acc + val
+//     })
+//     console.log(a2 / a.length);
+
+// }
+// solution([10, 20, 10, 2], [10, 25, 5, -2]  )
 
 
 
@@ -55,7 +55,39 @@ solution([10, 20, 10, 2], [10, 25, 5, -2]  )
 ////////////////33333333//////////////
 
 
-function permutations(string) {
-    
+function cakes(recipe, available) {
+    let a = []
+    for (i in recipe) {
+        if (!(i in available)) {
+           return 0
+        }
+        a.push(Math.floor(available[i] / recipe[i]))
+    }
+    return Math.min(...a)
+
+
 }
-permutations('aabb')
+
+console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}));
+console.log(cakes({ apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 }, { sugar: 500, flour: 2000, milk: 2000 }));
+
+
+
+
+///////////////444444444////////////////
+
+
+Math.round = function(number) {
+    return (number % 1 >= 0.5) ? number - number % 1 + 1 : number - number % 1;
+};
+Math.round(0.4)
+
+Math.ceil = function(number) {
+    return (number % 1 > 0) ? number - number % 1 + 1 : number;
+};
+Math.ceil(0.5)
+
+Math.floor = function(number) {
+    return number - number % 1;
+};
+Math.floor(0.5)
