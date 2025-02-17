408f080b39e108edb91b514d1b9fd25a1c058aa9
rustambekova <rustambekovaa15@gmail.com>
2025-02-17 22:42:13 +0600

    9_commit

---
+++ django-starter-main/a_rtchat/forms.py
@@ -0,0 +1,18 @@
+from django.forms import ModelForm
+
+from django import forms
+from .models import *
+
+
+class ChatmessagesCreateForm(ModelForm):
+    class Meta:
+        model = GroupMessage
+        fields = ['body']
+        widgets = {
+            'body': forms.TextInput(attrs={
+                'placeholder': "Add message .... ",
+                'class': 'p-4 text-black',
+                'maxlength': '300',
+                'autofocus': True
+            }),
+        }
--- django-starter-main/a_rtchat/views.py
+++ django-starter-main/a_rtchat/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import ChatmessagesCreateForm
+from .models import *
 # Create your views here.
+
+
+@login_required
 def chat_view(request):
-    return render(request='a_rtchat/chat.html')
+    chat_group = get_object_or_404(ChatGroup, group_name = 'publish-chat')
+    chat_messages = chat_group.chat_messages.all()[:30]
+    form  = ChatmessagesCreateForm()
+
+
+    if request.htmx:
+        form  =  ChatmessagesCreateForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            context = {
+                'message': message,
+                'user': request.user
+            }
+            return render (request, "a_rtchat/partials/chat_messages_p.html",context )
+
+    return render(request, 'a_rtchat/chat.html', {'chat_messages' : chat_messages, 'form' : form})
