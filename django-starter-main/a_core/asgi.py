dfcc6e7061afa4765f6c90256930e394660e9247
rustambekova <rustambekovaa15@gmail.com>
2025-02-18 23:09:37 +0600

    10_commit

--- django-starter-main/a_core/asgi.py
+++ django-starter-main/a_core/asgi.py
@@ -10,7 +10,24 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
 
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import  AuthMiddlewareStack
+django_asgi_app = get_asgi_application()
+
+from a_rtchat import routing
+
 application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+     "http":django_asgi_app,
+     "websocket": AllowedHostsOriginValidator(
+        
+          AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+     )
+    
+
+})
---
+++ django-starter-main/a_rtchat/routing.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .consumer import *
+
+
+websocket_urlpatterns = [
+    path('ws/chatroom/<chatroom_name>', ChatroomConsumer.as_asgi()),
+]
