6695bab765de0e95a54932359b2be621aedb01b9
rustambekova <rustambekovaa15@gmail.com>
2025-02-21 19:44:57 +0600

    13_commit

--- django-starter-main/a_core/settings.py
+++ django-starter-main/a_core/settings.py
@@ -99,6 +99,16 @@ CHANNEL_LAYERS = {
     }
 }
 
+
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [('redis://default:PtjiHTGONPhXwKCRqXYgvvBFWySGkCiu@redis.railway.internal:6379')],
+#         },
+#     },
+# }
+
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
--- django-starter-main/a_rtchat/consumer.py
+++ django-starter-main/a_rtchat/consumer.py
@@ -15,68 +15,73 @@ class ChatroomConsumer(WebsocketConsumer):
             self.chatroom_name, self.channel_name
         )
 
-        # add and update online users
+         # add and update online users
         if self.user not in self.chatroom.users_online.all():
+        
             self.chatroom.users_online.add(self.user)
             self.update_online_count()
-
-        self.accept()    
+        
+        self.accept()  
 
     def disconnect(self, close_code):
-            async_to_sync(self.channel_layer.group_discard)(
-                self.chatroom_name, self.channel_name
+        async_to_sync(self.channel_layer.group_discard)(
+            self.chatroom_name, self.channel_name
         )
         # remove and update online users
-
-            if self.user not in self.chatroom.users_online.all():
-                self.chatroom.users_online.remove(self.user)
-                self.update_online_count()
-    
-
-
+        if self.user  in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count()
 
 
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        body = text_data_json['body']
-
-        message = GroupMessage.objects.create(
-            body = body,
-            author = self.user,
-            group = self.chatroom
-        )
-
-        event = {
-            'type':'message_handler',
-            'message_id': message.id
-        }
-        
-
-        async_to_sync(self.channel_layer.group_send)(
-            self.chatroom_name, event
-        )
-
-    def message_handler(self,event):
+            text_data_json = json.loads(text_data)
+            body = text_data_json['body']
+            
+            message = GroupMessage.objects.create(
+                body = body,
+                author = self.user, 
+                group = self.chatroom 
+            )
+            event = {
+                'type': 'message_handler',
+                'message_id': message.id,
+            }
+            async_to_sync(self.channel_layer.group_send)(
+                self.chatroom_name, event
+            )
+            
+    def message_handler(self, event):
         message_id = event['message_id']
-        message = GroupMessage.objects.get(id = message_id)
+        message = GroupMessage.objects.get(id=message_id)
         context = {
-            "message":message,
-            "user": self.user,
+            'message': message,
+            'user': self.user,
+            'chat_group': self.chatroom
         }
-
-        html = render_to_string("a_rtchat/partials/chat_messages_p.html", context = context)
+        html = render_to_string("a_rtchat/partials/chat_messages_p.html", context=context)
         self.send(text_data=html)
-
-    
+        
+        
     def update_online_count(self):
-        online_count = self.chatroom.users_online.count()
+        online_count = self.chatroom.users_online.count() -1
+        
         event = {
-              'type':'online_count_handler',
-              'online_count':online_count
+            'type': 'online_count_handler',
+            'online_count': online_count
         }
-        async_to_sync(self.channel_layer.goup_send)(self.chatroom_name, event)
-
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
+        
     def online_count_handler(self, event):
         online_count = event['online_count']
-        html = render_to_string("a_rtchat/partials/online_count.html", {'online_count' : online_count})
-        self.send(text_data=html)
+        
+        chat_messages = ChatGroup.objects.get(group_name=self.chatroom_name).chat_messages.all()[:30]
+        author_ids = set([message.author.id for message in chat_messages])
+        users = User.objects.filter(id__in=author_ids)
+        
+        context = {
+            'online_count' : online_count,
+            'chat_group' : self.chatroom,
+            'users': users
+        }
+        html = render_to_string("a_rtchat/partials/online_count.html", context)
+        self.send(text_data=html) 
